e6c3e8e85eb2f21555737050e784b3bcacf6bd89
Azur Demirovic <abelnijeumro@gmail.com>
2026-03-22 22:45:48 +0100

    feat: add macro and conditional preprocessor directive

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#define MULTIPLY(a, b) ((a) * (b))
+
+#ifdef DEBUG
+    #define MESSAGE "Debug mode active"
+#else
+    #define MESSAGE "Production mode active"
+#endif
+
 void print_result(int res) {
     printf("Result: %d\n", res);
 }
@@ -7,8 +15,9 @@ void print_result(int res) {
 int main(void) {
     int x = 5;
     int y = 10;
-    int result = x * y;
+    int result = MULTIPLY(x, y);
 
+    printf("%s\n", MESSAGE);
     print_result(result);
 
     return 0;
